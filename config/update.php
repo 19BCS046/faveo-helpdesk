9508985e27b1982f8d497b58d6462967e1105b6b
sampleperu125 <sampleperu125@gmail.com>
2026-04-02 12:46:57 +0000

    update config

--- config/update.php
+++ config/update.php
@@ -3,7 +3,7 @@
 return [
 
     'github' => [
-        'owner' => env('GITHUB_REPO_OWNER', 'faveosuite'),
+        'owner' => env('GITHUB_REPO_OWNER', '19BCS046'),
         'repo'  => env('GITHUB_REPO_NAME', 'faveo-helpdesk'),
         'token' => env('GITHUB_ACCESS_TOKEN'),
     ],
